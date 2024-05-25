a5aca88dcb7e5e81ac4fa42dd73de6aab2f72c26
vasanth1066 <vasanthanuvavi@gmail.com>
2024-05-25 17:24:03 +0530

    Prerendering the powers of Next JS

--- app/[meetup]/page.tsx
+++ app/[meetup]/page.tsx
@@ -1,17 +1,49 @@
+import { notFound } from 'next/navigation';
 import Layout from '@/components/layout/Layout';
 import MeetUpDetails from '../../components/meetups/MeetUpDetails';
 
 
-const DisplayMeetupDetails=()=>{
-    return(
-        <Layout>
-        <MeetUpDetails  title="First MeetUp"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-AR0_HpWq1FgbKUsLiYUJm2QcQIfdU4C0Qw&s"
-        address="SomeWhere Under The Earth"></MeetUpDetails>
-        </Layout>
+async function fetchMeetupData(meetupId) {
 
-      
-    )
+  if (meetupId === 'm3') {
+    return null;
+  }
+
+  return {
+   
+    id: meetupId,
+    title:"First MeetUp",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-AR0_HpWq1FgbKUsLiYUJm2QcQIfdU4C0Qw&s",
+    address:"SomeWhere Under The Earth",
+  };
+}
+
+const DisplayMeetupDetails = async ({ params }) => {
+  const { meetupId } = params;
+  const meetupData = await fetchMeetupData(meetupId);
+
+  if (!meetupData) {
+    notFound();
+  }
+
+  return (
+    <Layout>
+      <MeetUpDetails 
+        title={meetupData.title}
+        image={meetupData.image}
+        address={meetupData.address}
+        description={meetupData.description}
+      />
+    </Layout>
+  );
+};
+
+export async function generateStaticParams() {
+  return [
+    { meetupId: 'm1' },
+    { meetupId: 'm2' },
+   
+  ];
 }
 
 export default DisplayMeetupDetails;
